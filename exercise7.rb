6baf66c4c8efba37eb49b88cbb91eb1a1525bbf4
Moufid Larbi <moufid4@gmail.com>
2014-08-13 17:38:38 -0400

    new function to calculate the total of students in all the cohorts

--- exercise7.rb
+++ exercise7.rb
@@ -7,7 +7,7 @@ new_students = {
 # Method to display the cohort name and amount of students	
 def cohort_display(students)
   students.each do |cohort, number|
-    puts "The cohort name is #{cohort} and the students number is #{number}"
+    puts "#{cohort}: #{number}"
   end
 end
 
@@ -24,14 +24,24 @@ puts new_students.keys.to_s
 
 
 #function to iterate through hash and increase cohort numbers by 5%
-def increase(new_students)
-new_students.each do |cohort, number|
+def increase_number(new_students)
+	new_students.each do |cohort, number|
 	new_students[cohort] = number * 1.05
+	end
 end
-end
-increase(new_students)
+increase_number(new_students)
 cohort_display(new_students)
 
 #delete the second cohort and display the cohort_display
 new_students.delete(:cohort2)
 cohort_display(new_students)
+
+#calculate total students 
+def total(new_students)
+	total_students = 0
+	new_students.each do |cohort, number|
+	total_students += number
+	end
+	puts total_students
+end
+total(new_students)
