cae6254f190da73c691fe1dc93e6478a0a4139cd
Moufid Larbi <moufid4@gmail.com>
2014-08-13 08:38:49 -0400

    Revert "Output rice"
    This reverts commit 797d629099950819ca7bc6522f7be2cd8c3d0f04.

--- exercise6.rb
+++ exercise6.rb
@@ -1,17 +1,10 @@
 grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
 
-# Added rice to grocery_list
 grocery_list.push("rice")
 puts "* #{grocery_list}"
 
-# Output rice from grocery_list
-grocery_list.delete("rice")
-puts "#{grocery_list}"
-
-
 grocery_list.each do |list|
 	puts "* #{list}"
-
 
 end
 
