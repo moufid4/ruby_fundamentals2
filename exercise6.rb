79941b7cede27b1a55c0af293c89233a04872933
Moufid Larbi <moufid4@gmail.com>
2014-08-13 08:35:26 -0400

    modified exercise6

--- exercise6.rb
+++ exercise6.rb
@@ -1,12 +1,27 @@
-grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
 
-# Added rice to grocery_list
-grocery_list.push("rice")
-puts "* #{grocery_list}"
+$grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
 
-# Output rice from grocery_list
-grocery_list.delete("rice")
-puts "#{grocery_list}"
+def grocery_add
+
+
+	puts "Please enter the name of the item you want to add/remove"
+		item_name = gets.chomp
+
+	$grocery_list.puts("fdsfds")
+puts grocery_list
+end
+
+
+puts "To add an item enter A - To remove an item enter B"
+item_add_remove=gets.chomp
+
+if item_add_remove="a"||"A" 
+	grocery_add
+
+elsif item_add_remove="r"||"R"
+ 	item_add_remove="r"||"R"
+	$grocery_list.delete("#{item_name}")
+end
 
 
 grocery_list.each do |list|
